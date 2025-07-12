3caf2478e6b95f275eada796014c7e33bda9f632
Victor Oliva <olivarra1@gmail.com>
2025-07-12 18:38:07 +0200

    code split tooltip

--- src/components/HintTooltip.tsx
+++ src/components/HintTooltip.tsx
@@ -1,15 +1,46 @@
 import { Info } from "lucide-react";
-import { TooltipContent, Tooltip, TooltipTrigger } from "./ui/tooltip";
-import type { FC, PropsWithChildren } from "react";
+import type * as TooltipModule from "./ui/tooltip";
+import { useEffect, useState, type FC, type PropsWithChildren } from "react";
 
-export const HintTooltip: FC<PropsWithChildren> = ({ children }) => (
-  <Tooltip>
-    <TooltipTrigger>
-      <Info size={18} className="text-foreground/80" />
-    </TooltipTrigger>
-    <TooltipContent className="max-w-[100vw]">{children}</TooltipContent>
-  </Tooltip>
-);
+let loadedModule: typeof TooltipModule | null = null;
+const tooltipPromise = import("./ui/tooltip").then((mod) => {
+  loadedModule = mod;
+  return mod;
+});
+const useModule = () => {
+  const [module, setModule] = useState(loadedModule);
+
+  useEffect(() => {
+    if (!module) {
+      tooltipPromise.then(setModule);
+    }
+  }, [module]);
+
+  return module;
+};
+
+export const HintTooltip: FC<PropsWithChildren> = ({ children }) => {
+  const module = useModule();
+
+  if (!module) {
+    return (
+      <button type="button" className="cursor-wait">
+        <Info size={18} className="text-foreground/80" />
+      </button>
+    );
+  }
+
+  const { Tooltip, TooltipTrigger, TooltipContent } = module;
+
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <Info size={18} className="text-foreground/80" />
+      </TooltipTrigger>
+      <TooltipContent className="max-w-[100vw]">{children}</TooltipContent>
+    </Tooltip>
+  );
+};
 
 export const TextHintTooltip: FC<{ hint: string }> = ({ hint }) => (
   <HintTooltip>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { analyzer } from "vite-bundle-analyzer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), process.env.ANALYZE ? analyzer() : null],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
