ecd4f78204aac48cdfd40c0b32bb28d856705574
Victor Oliva <olivarra1@gmail.com>
2025-07-12 18:17:51 +0200

    use default styles from shadcn for tooltip

--- src/components/HintTooltip.tsx
+++ src/components/HintTooltip.tsx
@@ -1,6 +1,5 @@
 import { Info } from "lucide-react";
-import { Tooltip, TooltipTrigger } from "./ui/tooltip";
-import { TooltipContent } from "@radix-ui/react-tooltip";
+import { TooltipContent, Tooltip, TooltipTrigger } from "./ui/tooltip";
 import type { FC, PropsWithChildren } from "react";
 
 export const HintTooltip: FC<PropsWithChildren> = ({ children }) => (
@@ -8,14 +7,12 @@ export const HintTooltip: FC<PropsWithChildren> = ({ children }) => (
     <TooltipTrigger>
       <Info size={18} className="text-foreground/80" />
     </TooltipTrigger>
-    <TooltipContent>{children}</TooltipContent>
+    <TooltipContent className="max-w-[100vw]">{children}</TooltipContent>
   </Tooltip>
 );
 
 export const TextHintTooltip: FC<{ hint: string }> = ({ hint }) => (
   <HintTooltip>
-    <p className="max-w-sm bg-background/60 backdrop-blur-xs p-2 rounded shadow">
-      {hint}
-    </p>
+    <p>{hint}</p>
   </HintTooltip>
 );
