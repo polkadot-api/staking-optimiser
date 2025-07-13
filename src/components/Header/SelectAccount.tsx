76b804f5161c03d21eb989a2ed90c72891ca8062
Victor Oliva <olivarra1@gmail.com>
2025-07-14 00:48:00 +0200

    code split account selector

--- src/components/Header/SelectAccount.tsx
+++ src/components/Header/SelectAccount.tsx
@@ -1,11 +1,3 @@
-import {
-  Dialog,
-  DialogBody,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { selectedAccount$ } from "@/state/account";
 import { identity$ } from "@/state/identity";
 import { withSubscribe } from "@/util/rxjs";
@@ -14,11 +6,10 @@ import { PolkadotIdenticon } from "@polkadot-api/react-components";
 import { state, useStateObservable } from "@react-rxjs/core";
 import { createSignal } from "@react-rxjs/utils";
 import { AccountId } from "polkadot-api";
-import { useState, type ReactElement } from "react";
+import { forwardRef, useState, type ReactElement } from "react";
 import { map, switchMap } from "rxjs";
 import { Button } from "../ui/button";
-import { ConnectSource } from "./ConnectSource";
-import { AccountSelector } from "./AccountSelector";
+import { codeSplit } from "@/util/codeSplit";
 
 const [openChange$, setOpen] = createSignal<boolean>();
 export const openSelectAccount = () => setOpen(true);
@@ -46,60 +37,87 @@ const selectedAccountName$ = selectedAccount$.pipeState(
   })
 );
 
-export const SelectAccount = withSubscribe(() => {
-  const open = useStateObservable(open$);
+const Trigger = forwardRef<HTMLButtonElement, object>((props, ref) => {
   const selectedAccount = useStateObservable(selectedAccount$);
   const accountName = useStateObservable(selectedAccountName$);
 
-  // Experimenting a bit but... who says I can't do this?
-  const [content, setContent] = useState<ReactElement | null>(null);
+  if (!selectedAccount || !accountName) return <Button>Connect</Button>;
 
-  const renderTrigger = () => {
-    if (!selectedAccount || !accountName) return <Button>Connect</Button>;
-
-    const publicKey =
-      selectedAccount.type === "address"
-        ? AccountId().enc(selectedAccount.value)
-        : selectedAccount.value.polkadotSigner.publicKey;
-
-    return (
-      <Button variant="outline">
-        <PolkadotIdenticon publicKey={publicKey} className="size-6" />
-        {accountName.name ? (
-          <div>{accountName.name}</div>
-        ) : (
-          <div className="text-sm text-foreground/60">
-            {sliceMiddleAddr(accountName.address)}
-          </div>
-        )}
-      </Button>
-    );
-  };
+  const publicKey =
+    selectedAccount.type === "address"
+      ? AccountId().enc(selectedAccount.value)
+      : selectedAccount.value.polkadotSigner.publicKey;
 
   return (
-    <Dialog
-      open={open}
-      onOpenChange={(open) => {
-        if (!open) {
-          setTimeout(() => setContent(null), 500);
-        }
-        setOpen(open);
-      }}
-    >
-      <DialogTrigger asChild>{renderTrigger()}</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Connect</DialogTitle>
-        </DialogHeader>
-        <DialogBody>
-          {content ?? (
-            <>
-              <AccountSelector />
-              <ConnectSource setContent={setContent} />
-            </>
-          )}
-        </DialogBody>
-      </DialogContent>
-    </Dialog>
+    <Button ref={ref} variant="outline" {...props}>
+      <PolkadotIdenticon publicKey={publicKey} className="size-6" />
+      {accountName.name ? (
+        <div>{accountName.name}</div>
+      ) : (
+        <div className="text-sm text-foreground/60">
+          {sliceMiddleAddr(accountName.address)}
+        </div>
+      )}
+    </Button>
   );
 });
+
+const payload = Promise.all([
+  import("@/components/ui/dialog"),
+  import("./ConnectSource"),
+  import("./AccountSelector"),
+]);
+export const SelectAccount = withSubscribe(
+  codeSplit(
+    payload,
+    () => <Trigger />,
+    ({ payload }) => {
+      const open = useStateObservable(open$);
+
+      // Experimenting a bit but... who says I can't do this?
+      const [content, setContent] = useState<ReactElement | null>(null);
+
+      const [
+        {
+          Dialog,
+          DialogBody,
+          DialogContent,
+          DialogHeader,
+          DialogTitle,
+          DialogTrigger,
+        },
+        { ConnectSource },
+        { AccountSelector },
+      ] = payload;
+
+      return (
+        <Dialog
+          open={open}
+          onOpenChange={(open) => {
+            if (!open) {
+              setTimeout(() => setContent(null), 500);
+            }
+            setOpen(open);
+          }}
+        >
+          <DialogTrigger asChild>
+            <Trigger />
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Connect</DialogTitle>
+            </DialogHeader>
+            <DialogBody>
+              {content ?? (
+                <>
+                  <AccountSelector />
+                  <ConnectSource setContent={setContent} />
+                </>
+              )}
+            </DialogBody>
+          </DialogContent>
+        </Dialog>
+      );
+    }
+  )
+);
--- src/components/HintTooltip.tsx
+++ src/components/HintTooltip.tsx
@@ -1,46 +1,29 @@
+import { codeSplit } from "@/util/codeSplit";
 import { Info } from "lucide-react";
-import type * as TooltipModule from "./ui/tooltip";
-import { useEffect, useState, type FC, type PropsWithChildren } from "react";
+import { type FC, type PropsWithChildren } from "react";
 
-let loadedModule: typeof TooltipModule | null = null;
-const tooltipPromise = import("./ui/tooltip").then((mod) => {
-  loadedModule = mod;
-  return mod;
-});
-const useModule = () => {
-  const [module, setModule] = useState(loadedModule);
+const module = import("./ui/tooltip");
 
-  useEffect(() => {
-    if (!module) {
-      tooltipPromise.then(setModule);
-    }
-  }, [module]);
+export const HintTooltip = codeSplit<Awaited<typeof module>, PropsWithChildren>(
+  module,
+  () => (
+    <button type="button" className="cursor-wait">
+      <Info size={18} className="text-foreground/80" />
+    </button>
+  ),
+  ({ children, payload }) => {
+    const { Tooltip, TooltipTrigger, TooltipContent } = payload;
 
-  return module;
-};
-
-export const HintTooltip: FC<PropsWithChildren> = ({ children }) => {
-  const module = useModule();
-
-  if (!module) {
     return (
-      <button type="button" className="cursor-wait">
-        <Info size={18} className="text-foreground/80" />
-      </button>
+      <Tooltip>
+        <TooltipTrigger>
+          <Info size={18} className="text-foreground/80" />
+        </TooltipTrigger>
+        <TooltipContent className="max-w-[100vw]">{children}</TooltipContent>
+      </Tooltip>
     );
   }
-
-  const { Tooltip, TooltipTrigger, TooltipContent } = module;
-
-  return (
-    <Tooltip>
-      <TooltipTrigger>
-        <Info size={18} className="text-foreground/80" />
-      </TooltipTrigger>
-      <TooltipContent className="max-w-[100vw]">{children}</TooltipContent>
-    </Tooltip>
-  );
-};
+);
 
 export const TextHintTooltip: FC<{ hint: string }> = ({ hint }) => (
   <HintTooltip>
