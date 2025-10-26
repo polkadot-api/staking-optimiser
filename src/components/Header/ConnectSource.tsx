f990687b5bf70d6a437975a4e310358e7bf1b664
Victor Oliva <olivarra1@gmail.com>
2025-10-27 00:59:00 +0100

    move account selection to polkahub

--- src/components/Header/ConnectSource.tsx
+++ src/components/Header/ConnectSource.tsx
@@ -1,141 +1,63 @@
 import { cn } from "@/lib/utils";
-import { pjsWalletProvider } from "@/state/account";
-import { useStateObservable } from "@react-rxjs/core";
-import { CircleQuestionMark, Eye } from "lucide-react";
-import type { FC, MouseEvent, PropsWithChildren, ReactElement } from "react";
+import { Eye } from "lucide-react";
+import { ModalContext } from "polkahub";
+import {
+  useContext,
+  type FC,
+  type MouseEvent,
+  type PropsWithChildren,
+} from "react";
 import { LedgerAccounts } from "../LedgerAccounts";
 import { Button } from "../ui/button";
 import { VaultAccounts } from "../vault/VaultAccounts";
 import { ManageAddresses } from "./ManageAddresses";
 
-const knownExtensions: Record<string, { name: string; logo: string }> = {
-  "polkadot-js": {
-    name: "Polkadot JS",
-    logo: import.meta.env.BASE_URL + "polkadotjs.webp",
-  },
-  "nova-wallet": {
-    name: "Nova Wallet",
-    logo: import.meta.env.BASE_URL + "novawallet.webp",
-  },
-  talisman: {
-    name: "Talisman",
-    logo: import.meta.env.BASE_URL + "talisman.webp",
-  },
-  "subwallet-js": {
-    name: "Subwallet",
-    logo: import.meta.env.BASE_URL + "subwallet.webp",
-  },
-};
-
-export const ConnectSource: FC<{
-  setContent: (element: ReactElement | null) => void;
-}> = ({ setContent }) => {
-  const availableExtensions = useStateObservable(
-    pjsWalletProvider.availableExtensions$
-  ).sort(
-    (a, b) => (b in knownExtensions ? 1 : 0) - (a in knownExtensions ? 1 : 0)
-  );
+export const ConnectSource: FC = () => {
+  const { setContent } = useContext(ModalContext)!;
 
   return (
-    <div className="space-y-4">
-      {availableExtensions.length ? (
-        <div>
-          <h3>Manage Extensions</h3>
-          <ul className="flex gap-2 flex-wrap items-center justify-center">
-            {availableExtensions.map((id) => (
-              <li key={id}>
-                <ExtensionButton id={id} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : null}
-      <div>
-        <h3>Manage Connections</h3>
-        <div className="flex gap-2 flex-wrap items-center justify-center">
-          <SourceButton
-            label="Address"
-            onClick={() =>
-              setContent(<ManageAddresses onClose={() => setContent(null)} />)
-            }
-          >
-            <div>
-              <Eye className="size-10" />
-            </div>
-          </SourceButton>
-          <SourceButton
-            label="Ledger"
-            onClick={() =>
-              setContent(<LedgerAccounts setContent={setContent} />)
-            }
-          >
-            <img
-              src={import.meta.env.BASE_URL + "ledger.webp"}
-              alt="Ledger"
-              className="h-10 rounded"
-            />
-          </SourceButton>
-          <SourceButton
-            label="Vault"
-            onClick={() =>
-              setContent(<VaultAccounts setContent={setContent} />)
-            }
-          >
-            <img
-              src={import.meta.env.BASE_URL + "vault.webp"}
-              alt="Vault"
-              className="h-10 rounded"
-            />
-          </SourceButton>
-          <SourceButton label="Wallet Connect" disabled>
-            <img
-              src={import.meta.env.BASE_URL + "walletConnect.svg"}
-              alt="Vault"
-              className="h-10 rounded"
-            />
-          </SourceButton>
-        </div>
+    <div>
+      <h3>Manage Connections</h3>
+      <div className="flex gap-2 flex-wrap items-center justify-center">
+        <SourceButton
+          label="Address"
+          onClick={() =>
+            setContent(<ManageAddresses onClose={() => setContent(null)} />)
+          }
+        >
+          <div>
+            <Eye className="size-10" />
+          </div>
+        </SourceButton>
+        <SourceButton
+          label="Ledger"
+          onClick={() => setContent(<LedgerAccounts setContent={setContent} />)}
+        >
+          <img
+            src={import.meta.env.BASE_URL + "ledger.webp"}
+            alt="Ledger"
+            className="h-10 rounded"
+          />
+        </SourceButton>
+        <SourceButton
+          label="Vault"
+          onClick={() => setContent(<VaultAccounts setContent={setContent} />)}
+        >
+          <img
+            src={import.meta.env.BASE_URL + "vault.webp"}
+            alt="Vault"
+            className="h-10 rounded"
+          />
+        </SourceButton>
+        <SourceButton label="Wallet Connect" disabled>
+          <img
+            src={import.meta.env.BASE_URL + "walletConnect.svg"}
+            alt="Vault"
+            className="h-10 rounded"
+          />
+        </SourceButton>
       </div>
     </div>
-  );
-};
-
-const ExtensionButton: FC<{
-  id: string;
-}> = ({ id }) => {
-  const knownExtension = knownExtensions[id];
-  const connectedExtensions = useStateObservable(
-    pjsWalletProvider.connectedExtensions$
-  );
-  const isSelected = connectedExtensions.includes(id);
-
-  return (
-    <SourceButton
-      isSelected={isSelected}
-      label={knownExtension?.name ?? id}
-      onClick={() =>
-        pjsWalletProvider.setConnectedExtensions(
-          isSelected
-            ? connectedExtensions.filter((v) => v !== id)
-            : [...connectedExtensions, id]
-        )
-      }
-    >
-      {knownExtension ? (
-        <img
-          src={knownExtension.logo}
-          alt={knownExtension.name}
-          className="h-10 rounded"
-        />
-      ) : (
-        <div>
-          <CircleQuestionMark
-            className="size-10 text-muted-foreground"
-            strokeWidth={1}
-          />
-        </div>
-      )}
-    </SourceButton>
   );
 };
 
--- src/components/Header/SelectAccount.tsx
+++ src/components/Header/SelectAccount.tsx
@@ -1,141 +1,32 @@
-import { cn } from "@/lib/utils";
-import { selectedAccountPlugin } from "@/state/account";
-import { identity$ } from "@/state/identity";
 import { codeSplit } from "@/util/codeSplit";
 import { withSubscribe } from "@/util/rxjs";
-import { sliceMiddleAddr } from "@/util/ss58";
-import { PolkadotIdenticon } from "@polkadot-api/react-components";
-import { state, useStateObservable } from "@react-rxjs/core";
-import { createSignal } from "@react-rxjs/utils";
-import { AccountId } from "polkadot-api";
-import { useSelectedAccount } from "polkahub";
-import {
-  forwardRef,
-  useState,
-  type ComponentProps,
-  type ReactElement,
-} from "react";
-import { map, switchMap } from "rxjs";
-import { Button } from "../ui/button";
-
-const [openChange$, setOpen] = createSignal<boolean>();
-export const openSelectAccount = () => setOpen(true);
-const open$ = state(openChange$, false);
-
-const selectedAccountName$ = selectedAccountPlugin.selectedAccount$.pipeState(
-  switchMap((v) => {
-    if (!v) return [null];
-
-    if (v.name) {
-      return [
-        {
-          name: v.name || null,
-          address: v.address,
-        },
-      ];
-    }
-
-    return identity$(v.address).pipe(
-      map((id) => ({
-        name: id ? id.value + (id.subId ? `/${id.subId}` : "") : null,
-        address: v.address,
-      }))
-    );
-  })
-);
-
-const Trigger = forwardRef<
-  HTMLButtonElement,
-  ComponentProps<typeof Button> & {
-    loading?: boolean;
-  }
->(({ loading, ...props }, ref) => {
-  const [selectedAccount] = useSelectedAccount();
-  const accountName = useStateObservable(selectedAccountName$);
-
-  if (!selectedAccount || !accountName)
-    return (
-      <Button ref={ref} {...props}>
-        Connect
-      </Button>
-    );
-
-  const publicKey = AccountId().enc(selectedAccount.address);
-
-  return (
-    <Button
-      ref={ref}
-      variant="outline"
-      {...props}
-      className={cn(loading ? "cursor-wait" : null, props.className)}
-    >
-      <PolkadotIdenticon publicKey={publicKey} className="size-6" />
-      {accountName.name ? (
-        <div>{accountName.name}</div>
-      ) : (
-        <div className="text-sm text-foreground/60">
-          {sliceMiddleAddr(accountName.address)}
-        </div>
-      )}
-    </Button>
-  );
-});
+import { SelectAccountModalTrigger } from "polkahub";
 
 const payload = Promise.all([
-  import("@/components/ui/dialog"),
+  import("polkahub"),
   import("./ConnectSource"),
-  import("./AccountSelector"),
+  import("polkahub").then(({ AccountSelector, ManagePjsWallets }) => ({
+    AccountSelector,
+    ManagePjsWallets,
+  })),
 ]);
 export const SelectAccount = withSubscribe(
   codeSplit(
     payload,
-    () => <Trigger loading />,
+    () => <SelectAccountModalTrigger />,
     ({ payload }) => {
-      const open = useStateObservable(open$);
-
-      // Experimenting a bit but... who says I can't do this?
-      const [content, setContent] = useState<ReactElement | null>(null);
-
       const [
-        {
-          Dialog,
-          DialogBody,
-          DialogContent,
-          DialogHeader,
-          DialogTitle,
-          DialogTrigger,
-        },
+        { SelectAccountModal },
         { ConnectSource },
-        { AccountSelector },
+        { AccountSelector, ManagePjsWallets },
       ] = payload;
 
       return (
-        <Dialog
-          open={open}
-          onOpenChange={(open) => {
-            if (!open) {
-              setTimeout(() => setContent(null), 500);
-            }
-            setOpen(open);
-          }}
-        >
-          <DialogTrigger asChild>
-            <Trigger />
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Connect</DialogTitle>
-            </DialogHeader>
-            <DialogBody>
-              {content ?? (
-                <>
-                  <AccountSelector />
-                  <ConnectSource setContent={setContent} />
-                </>
-              )}
-            </DialogBody>
-          </DialogContent>
-        </Dialog>
+        <SelectAccountModal>
+          <AccountSelector />
+          <ManagePjsWallets />
+          <ConnectSource />
+        </SelectAccountModal>
       );
     }
   )
