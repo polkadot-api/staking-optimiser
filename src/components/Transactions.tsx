23e652e8a92bbab937e158aac6947ccf23b7ab32
Victor Oliva <olivarra1@gmail.com>
2025-10-31 17:06:05 +0100

    add TOS

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -1,10 +1,24 @@
 import { selectedSignerAccount$ } from "@/state/account";
+import { createLocalStorageState } from "@/util/rxjs";
 import type { AsyncTransaction } from "@polkadot-api/sdk-staking";
+import { Checkbox } from "@polkahub/ui-components";
 import { shareLatest, useStateObservable } from "@react-rxjs/core";
 import { Eye, Loader2, Zap } from "lucide-react";
-import { InvalidTxError, type Transaction, type TxEvent } from "polkadot-api";
-import { lazy, useState, type ComponentType, type FC } from "react";
+import {
+  InvalidTxError,
+  type PolkadotSigner,
+  type Transaction,
+  type TxEvent,
+} from "polkadot-api";
+import {
+  lazy,
+  useState,
+  type ComponentType,
+  type FC,
+  type PropsWithChildren,
+} from "react";
 import { from, lastValueFrom, switchMap, type Observable } from "rxjs";
+import { DialogButton } from "./DialogButton";
 import { Button } from "./ui/button";
 
 const toastModule = import("react-toastify");
@@ -14,7 +28,7 @@ const ToastContainer = lazy(() =>
 );
 
 // Error invalid fee keeps the toast open
-export function trackTransaction(tx$: Observable<TxEvent>) {
+function trackTransaction(tx$: Observable<TxEvent>) {
   return from(toastModule).pipe(
     switchMap(({ toast }) => {
       const shared$ = tx$.pipe(shareLatest());
@@ -79,7 +93,7 @@ export function trackTransaction(tx$: Observable<TxEvent>) {
 
 export const Transactions = () => <ToastContainer position="bottom-right" />;
 
-export const useSingleTransaction = () => {
+const useSingleTransaction = () => {
   const [isPending, setIsPending] = useState(false);
 
   const startTx = async (tx$: Observable<TxEvent>) => {
@@ -94,9 +108,15 @@ export const useSingleTransaction = () => {
   return [isPending, startTx] as const;
 };
 
+const [hasAccepted$, setHasAccepted] = createLocalStorageState(
+  "tos-accepted",
+  false
+);
+
 type ButtonProps = typeof Button extends ComponentType<infer R> ? R : never;
 
 type Awaitable<T> = T | Promise<T>;
+
 export const TransactionButton: FC<
   ButtonProps & {
     createTx: () => Awaitable<
@@ -105,40 +125,118 @@ export const TransactionButton: FC<
     onSuccess?: () => void;
     onError?: (err: any) => void;
   }
-> = ({ createTx, children, onSuccess, onError, ...props }) => {
+> = ({ createTx, onSuccess, onError, ...props }) => {
+  const hasAccepted = useStateObservable(hasAccepted$);
   const account = useStateObservable(selectedSignerAccount$);
   const [isOngoing, trackTx] = useSingleTransaction();
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const signer = account?.polkadotSigner;
 
-  return (
-    <Button
+  const sign = async () => {
+    setIsSubmitting(true);
+    try {
+      const tx = await createTx();
+      if (!tx) return;
+      await trackTx(tx.signSubmitAndWatch(signer!));
+      onSuccess?.();
+    } catch (ex) {
+      console.error(ex);
+      onError?.(ex);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return hasAccepted ? (
+    <AcceptedTransactionButton
       {...props}
-      disabled={!signer || isOngoing || isSubmitting || props.disabled}
-      onClick={async () => {
-        setIsSubmitting(true);
-        try {
-          const tx = await createTx();
-          if (!tx) return;
-          await trackTx(tx.signSubmitAndWatch(signer!));
-          onSuccess?.();
-        } catch (ex) {
-          console.error(ex);
-          onError?.(ex);
-        } finally {
-          setIsSubmitting(false);
-        }
-      }}
+      isOngoing={isOngoing}
+      disabled={isSubmitting || props.disabled}
+      onClick={sign}
+    />
+  ) : (
+    <TermsOfServiceButton {...props} sign={sign} />
+  );
+};
+
+const TermsOfServiceButton: FC<PropsWithChildren<{ sign: () => void }>> = ({
+  children,
+  sign,
+}) => {
+  const account = useStateObservable(selectedSignerAccount$);
+  const signer = account?.polkadotSigner;
+  const [accepted, setAccepted] = useState(false);
+
+  return (
+    <DialogButton
+      title="Terms of service"
+      content={({ close }) => (
+        <div className="space-y-2 text-sm">
+          <p>
+            By using this application, you acknowledge and agree to the terms of{" "}
+            <a
+              className="underline"
+              target="blank"
+              href="https://github.com/polkadot-api/staking-optimiser/blob/main/LICENSE"
+            >
+              its license
+            </a>
+            .
+          </p>
+          <p>
+            This application and its developers provide <b>no warranty</b> and
+            accept <b>no liability</b> for any losses or damages arising from
+            its use. Always <b>verify all information shown</b>, and{" "}
+            <b>check any transactions</b> before signing them.
+          </p>
+          <label className="flex items-center gap-1">
+            <Checkbox
+              required
+              checked={accepted}
+              onCheckedChange={(evt) => setAccepted(evt === true)}
+            />
+            I have read and agree to the above
+          </label>
+          <div className="flex justify-between">
+            <Button
+              disabled={!accepted}
+              onClick={() => {
+                setHasAccepted(true);
+                sign();
+              }}
+            >
+              Accept
+            </Button>
+            <Button variant="secondary" onClick={close}>
+              Cancel
+            </Button>
+          </div>
+        </div>
+      )}
     >
       {children}
-      {isOngoing ? (
-        <Loader2 className="animate-spin" />
-      ) : signer ? (
-        <Zap />
-      ) : (
-        <Eye />
-      )}
+      <TxButtonLogo signer={signer} />
+    </DialogButton>
+  );
+};
+
+const AcceptedTransactionButton: FC<
+  PropsWithChildren<ButtonProps & { isOngoing: boolean }>
+> = ({ children, isOngoing, ...props }) => {
+  const account = useStateObservable(selectedSignerAccount$);
+  const signer = account?.polkadotSigner;
+
+  return (
+    <Button {...props} disabled={!signer || isOngoing || props.disabled}>
+      {children}
+      <TxButtonLogo isOngoing={isOngoing} signer={signer} />
     </Button>
   );
 };
+
+const TxButtonLogo: FC<{ isOngoing?: boolean; signer?: PolkadotSigner }> = ({
+  isOngoing,
+  signer,
+}) =>
+  isOngoing ? <Loader2 className="animate-spin" /> : signer ? <Zap /> : <Eye />;
