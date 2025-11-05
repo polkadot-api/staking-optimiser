e673c229da2af4f07f28db9b98b06c268c612681
Victor Oliva <olivarra1@gmail.com>
2025-11-05 17:06:28 +0100

    add vercel skew protection

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { analyzer } from "vite-bundle-analyzer";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { analyzer } from "vite-bundle-analyzer"
+// @ts-expect-error
+import vercelSkewProtection from "vite-plugin-vercel-skew-protection"
+
+const skewProtecEnabled = process.env.VERCEL_SKEW_PROTECTION_ENABLED === "1"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), process.env.ANALYZE ? analyzer() : null],
+  plugins: [
+    react(),
+    tailwindcss(),
+    skewProtecEnabled ? vercelSkewProtection() : null,
+    process.env.ANALYZE ? analyzer() : null,
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -15,4 +24,4 @@ export default defineConfig({
   worker: {
     format: "es",
   },
-});
+})
